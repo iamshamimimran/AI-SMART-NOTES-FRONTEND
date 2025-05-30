e1210bc5bc637bf96d1d0ea2fbcd8f4cf6f2fb02
Shamim Imran <shamimimran35@gmail.com>
2025-05-30 12:01:28 +0530

    Update API base URL to use production endpoint

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // import { API_BASE_URL } from "../utils/api";
-const API_BASE_URL = "http://localhost:5000/";
+const API_BASE_URL = "https://ai-smart-notes-backend.onrender.com/";
 // Register user
 export const registerUser = async (userData) => {
   const response = await axios.post(
